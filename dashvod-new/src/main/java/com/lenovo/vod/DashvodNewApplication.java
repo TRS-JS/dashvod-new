583195ccc574cbbe8578edad9bb95e1f615d58b2
叶浩 <storv2580@163.com>
2017-04-11 11:02:59 +0800

    初始化

---
+++ dashvod-new/src/main/java/com/lenovo/vod/DashvodNewApplication.java
@@ -0,0 +1,12 @@
+package com.lenovo.vod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashvodNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashvodNewApplication.class, args);
+	}
+}
---
+++ dashvod-new/src/test/java/com/lenovo/vod/DashvodNewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lenovo.vod;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DashvodNewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
